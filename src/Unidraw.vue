ce5f7fa681f2b092b1376b6611aa6904b89eaa6f
Terry G Lorber <terry@metatooth.com>
2020-08-26 12:20:47 -0400

    working

--- src/Unidraw.vue
+++ src/Unidraw.vue
@@ -51,6 +51,7 @@ export default {
     };
   },
   mounted() {
+    console.log(process.env);
     const commit = process.env.HEROKU_SLUG_COMMIT ?
       process.env.HEROKU_SLUG_COMMIT.substring(0, 7) : '19740621';
     this.version = 'DEV ' + commit;
