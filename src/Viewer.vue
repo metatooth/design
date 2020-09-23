e2494c41caef95b99779445e7cc94f52410e20a9
Terry G Lorber <terry@metatooth.com>
2020-09-23 15:14:49 -0400

    working

--- src/Viewer.vue
+++ src/Viewer.vue
@@ -29,7 +29,6 @@
  * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-import {AxesHelper} from 'three';
 import {AmbientLight} from 'three';
 import {DirectionalLight} from 'three';
 import {Object3D} from 'three';
@@ -146,8 +145,6 @@ export default {
       this.initRenderer();
 
       this.initControls();
-
-      this.scene.add( new AxesHelper(50) );
 
       window.addEventListener( 'resize', this.isResized, false );
     },
